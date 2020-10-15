dd76b7723a33b54089571dd4f278230cc52d33d4
Karan Bhargava <karan.bhargava@metacube.com>
2020-10-15 21:50:00 +0530

    Update comments

--- Docker-Intermediate/ShellScript/list-folders.sh
+++ Docker-Intermediate/ShellScript/list-folders.sh
@@ -3,8 +3,11 @@
 function print_tree() {
 	separator="$1"
 	shift
+	#the parent directory
 	parent="$1"
 	shift
+
+	#iterate over the sub directories
 	for sub_dir
 	do
 		if [ "${sub_dir}" != "" ]
@@ -13,10 +16,15 @@ function print_tree() {
 			current_dir="${parent}/${sub_dir}"
 			if [ -d "${current_dir}" ]
 			then
+				#desribe the contents of folder
 				folder_descriptor="$(ls -A "${current_dir}")"
+				#convert it to array
 				readarray -t sub_dirs <<< "${folder_descriptor}"
+				#recursively call
 				print_tree "${separator}--" "${current_dir}" "${sub_dirs[@]}"
+				#backtrack
 				parent="${parent}/.."
+				#update the separator along with parent
 				separator="${separator%--}"
 			fi
 		fi
